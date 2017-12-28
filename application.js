caa42e58f24840a48040445f95da862d4d65a74d
mabrsweeney <mabrsweeney@gmail.com>
2017-12-28 15:50:44 -0800

    streamlined application.js

--- application.js
+++ application.js
@@ -1,11 +1,9 @@
-var latestTweetIndex;
-
-$(document).ready(function(){
-
-  var $body = $('body');
-  var index = streams.home.length - 1;
+var $body = $('body');
+var index = streams.home.length - 1;
   var latestTweetIndex = 0;
-  while(latestTweetIndex <= index){
+function getTweets(){
+
+  while(latestTweetIndex < index){
     var tweet = streams.home[latestTweetIndex];
     var $tweet = $('<div></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message + '; ' + tweet.created_at);
@@ -13,17 +11,14 @@ $(document).ready(function(){
     latestTweetIndex += 1;
 
   }
+}
+$(document).ready(function(){
+
+  getTweets();
 
   $('#getNewTweets').on('click',function(){
     index = streams.home.length - 1;
-    while(latestTweetIndex < index){
-      var tweet = streams.home[latestTweetIndex];
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + '; ' + tweet.created_at);
-      $tweet.appendTo($body);
-      latestTweetIndex += 1;
-
-  }
+    getTweets();
     latestTweetIndex = index;
   });
 });
