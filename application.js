df28aebf20ed4789542dbe638b123afea05d0469
mabrsweeney <mabrsweeney@gmail.com>
2017-12-29 10:14:16 -0800

    added styles.css; improved main page

--- application.js
+++ application.js
@@ -1,24 +1,36 @@
-var $body = $('body');
+var $tweets = $('.tweetSection');
 var index = streams.home.length - 1;
-  var latestTweetIndex = 0;
-function getTweets(){
+var latestTweetIndex = 0;
 
+
+function getTweets(){
   while(latestTweetIndex < index){
     var tweet = streams.home[latestTweetIndex];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + '; ' + tweet.created_at);
-    $tweet.appendTo($body);
+    var $tweet = $('<div class=\'tweet\'></div>');
+    var $user = $('<p class=\'user\'></p>');
+    var $message = $('<p class=\'message\'></p>');
+    var $timeSig = $('<p class=\'timeSig\'></p>');
+    $user.text('@' + tweet.user + ': ');
+    $message.text(tweet.message + '; ');
+    $timeSig.text(tweet.created_at)
+    $tweet.append($user).append($message).append($timeSig);
+    $tweet.prependTo($tweets);
+    $tweet.show('slow', function(){});
     latestTweetIndex += 1;
 
   }
 }
+
 $(document).ready(function(){
 
-  getTweets();
-
-  $('#getNewTweets').on('click',function(){
+  setInterval(function(){
     index = streams.home.length - 1;
     getTweets();
     latestTweetIndex = index;
+  },100);
+
+  $('.tweet').on('click', function(){
+    console.log('egg');
   });
+
 });
